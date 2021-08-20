7ffeb8963e0498cf06036244f9c8a68e65e51839
Philipp Wendt <philipp.wendt@neuefische.de>
2021-08-20 11:21:25 +0200

    Change login credentials, implement feedback

--- sql.py
+++ sql.py
@@ -6,14 +6,6 @@ user='__________'
 password='______'
 
 ## Do NOT change anything below!
-# psycopg2 connection for querying data from a database
-import psycopg2
-conn = psycopg2.connect(host=host,
-                        port=port,
-                        database=database,
-                        user=user,
-                        password=password)
-
 # sqlalchemy engine for writing data to a database
 from sqlalchemy import create_engine    
 engine = create_engine(f'postgres+psycopg2://{user}:{password}@{host}:{port}/{database}')
